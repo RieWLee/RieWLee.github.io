464c07d3de3714b644202c37783e9ea75e6fb61b
Jupitrie <riegrindall@Ries-MacBook-Air.local>
2017-07-17 21:13:37 -0700

    added new css

--- pop/js/main.js
+++ pop/js/main.js
@@ -1,20 +1,23 @@
-//create 2 functions?
-$('#final').click(function(){
-	$('#final').html('You clicked me!');
-	$('#final').css('color', 'pink');
-});
 
-//compare the number that user inputs to the number that the computer prefers
+var girlStory = document.getElementById("f-option").checked;
+var boyStory = document.getElementById("m-option").checked;
+var otherStory = document.getElementById("x-option").checked;
 
-function promptMe(){
-var inputText = prompt("What is the number?");
-};
-var inputComputer = 5;
+$("#submit").on("click", function(){
+  if (girlStory = true) {
+    window.open(story1.html);
+  } else if (boyStory = true){
+    window.open(story2.html);
+  } else {
+    window.open(story3.html);
+  }
+})
 
-	if (inputText === inputComputer) {
-		alert('Great minds think alike!')
-	} else if (inputText < inputComputer) {
-		alert('Five is clearly the superior number.')
-	} else {
-		alert("Wow! SHOW OFF!!");
-	}
+
+var charaName = document.getElementById("name");
+
+  $("span, p").each(function() {
+      var nameReplace = $(charaName).text();
+      nameReplace = text.replace("userfirstname");
+      $(this).text(text);
+  });
