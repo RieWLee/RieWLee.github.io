6837456ec0cef0805a43396cc14c763727879df8
Jupitrie <riegrindall@Ries-MacBook-Air.local>
2017-07-24 14:34:15 -0700

    Adding Children Book images

--- pop/js/main.js
+++ pop/js/main.js
@@ -1,10 +1,29 @@
+
+window.onload = function() {
+
+        // If sessionStorage is storing default values (ex. name), exit the function and do not restore data
+        if (sessionStorage.getItem("name") == "name") {
+            return;
+        }
+        // If values are not blank, restore them to the fields
+        var name = sessionStorage.getItem("name");
+        if (name !== null) $("#name").val(name);
+    }
+
+    // Before refreshing the page, save the form data to sessionStorage
+    window.onbeforeunload = function() {
+        sessionStorage.setItem("name", $('#name').val(charaName));
+    }
 var charaName = document.getElementById("name");
 
-console.log (charaName)
+window.onload = function() {
+    var name = localStorage.getItem(charaName);
+    if (name !== null) $('#name').val(name);
+}
 
 $(".farstory h2").each(function() {
    var text = $(this).html();
-   $(this).html(text.replace("userf1rstname", '+charaName+'));
+   $(this).html(text.replace("userf1rstname", " + charaName + "));
 });
 
 var gender = document.getElementById("gender").value;
