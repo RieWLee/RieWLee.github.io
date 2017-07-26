c25ab3fa00c750cea113aa649194872c277ffd3f
Jupitrie <riegrindall@Ries-MacBook-Air.local>
2017-07-26 14:42:22 -0700

    adding working javascript/jquery!

--- pop/js/main.js
+++ pop/js/main.js
@@ -1,44 +1,20 @@
-//
-// window.onload = function() {
-//
-//         // If sessionStorage is storing default values (ex. name), exit the function and do not restore data
-//         if (sessionStorage.getElementById("name") == "name") {
-//             return;
-//         }
-//         // If values are not blank, restore them to the fields
-//         var name = sessionStorage.getElementById("name");
-//         if (name !== null) $("#name").val(name);
-//     }
-//
-//     // Before refreshing the page, save the form data to sessionStorage
-//     window.onbeforeunload = function() {
-//         sessionStorage.getElementById("name", $('#name').val(name));
-//     }
-//
-//
-// window.onload = function() {
-//     var name = localStorage.getItem(name);
-//     if (name !== null) $('#name').val(name);
-// }
-//
-// $(".farstory h2").each(function() {
-// 		var charaName = sessionStorage.getElementById("name");
-//    	var text = $(this).html();
-//    $(this).html(text.replace("userf1rstname", charaName));
-// });
-//
-//
 window.localStorage.clear(); //try this to clear all local storage
 
-$("#storytime").on("submit",function(e){
-	e.preventDefault();
-	if (document.getElementById("story1").checked) {
-	location.href = "story1.html";
-} else if (document.getElementById("story2").checked) {
-	location.href = "story2.html";
-} else (document.getElementById("story3").checked)
-	location.href = "story3.html";
+$(function(){
+    $("#storytime").submit(function(e) {
+        e.preventDefault();
+        window.location = $(this).find('input[type="radio"]:checked').val();
+    });
 });
+// $("#storytime").on("submit",function(e){
+// 	e.preventDefault();
+// 	if (document.getElementById("story1").checked) {
+// 	window.location = "story1.html";
+// } else if (document.getElementById("story2").checked) {
+// 	window.location = "story2.html";
+// } else (document.getElementById("story3").checked)
+// 	window.location = "story3.html";
+// });
 
 $(document).on("click", "#submit", function(e) {
   var stuff = ($("#name").val());
@@ -54,15 +30,6 @@ $(document).on("click", "#submit", function(e) {
   }
 
 });
-$(document).on("click", "#submit", function(e) {
-  alert(JSON.parse(localStorage.getItem('namey')));
-});
-
-//
-//
-//
-//   // $("span, p").each(function() {
-//   //     var nameReplace = $(charaName).text();
-//   //     nameReplace = text.replace("userfirstname");
-//   //     $(this).text(text);
-//   // });
+// $(document).on("click", "#submit", function(e) {
+//   alert(JSON.parse(localStorage.getItem('namey')));
+// });
