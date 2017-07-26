7a2caac129824b7dd32529686893188078c7dd6e
Jupitrie <riegrindall@Ries-MacBook-Air.local>
2017-07-26 16:15:49 -0700

    adding a few extra polishes

--- pop/js/story.js
+++ pop/js/story.js
@@ -9,10 +9,15 @@ window.onload = function() {
 
     var charaName = localStorage.getItem("namey");
     if (name !== null) document.getElementById("name");
-
+		charaName.toString();
     // ...
 		$(".farstory h2").each(function() {
 		   	var text = $(this).html();
-		   $(this).html(text.replace("userf1rstname", charaName));
+		   $(this).html(text.replace("userf1rstname",charaName));
 		});
 }
+app.filter('clearText', function() {
+			 return function(text) {
+				 return  text ? charaName(text).replace(/"<[^>]+>/gm, '') : '';
+			}
+		 });
